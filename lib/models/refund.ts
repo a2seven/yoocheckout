c081f0e6b5c308e74d8a53633897e99acaa9a375
Nikolay Martynenko <martynenko.nikolay@gmail.com>
2023-03-18 03:47:57 +0300

    Update Refund object field types

--- lib/models/refund.ts
+++ lib/models/refund.ts
@@ -1,22 +1,29 @@
 import {
     IAmount,
     IReceipt,
+    IReceiptStatus,
+    IRefundCancellationParty,
+    IRefundCancellationReason,
     IRefundStatus,
     ISource
-} from "../types";
+} from '../types';
 
 export class Refund {
     id!: string;
     payment_id!: string;
     status!: IRefundStatus;
+    cancellation_details!: {
+        party: IRefundCancellationParty;
+        reason: IRefundCancellationReason;
+    };
+    receipt_registration!: IReceiptStatus;
     created_at!: string;
     amount!: IAmount;
     description!: string;
     sources!: ISource[];
     receipt?: IReceipt;
-    constructor() { }
+    constructor() {}
 }
-
 
 export const refundFactory = (payload: any): Refund => {
     return Object.assign(new Refund(), payload);
